8715ae080213dbb21f296678cd8ccfa6167a6ed6
Irina Lukyanova <runnaf@mail.ru>
2022-05-13 15:53:48 +0300

    Задание первой недели

---
+++ index.js
@@ -0,0 +1,100 @@
+/*alert('Это моя первая строчка кода в JavaScript!');
+let books = 7;
+alert(books);
+
+let country = "Italy";
+alert(country);
+
+const AGE = 25;
+const ROOM = 5;
+let clothes = 'dress';
+
+let apples = 5;
+apples = 3;
+apples = 2;
+alert(apples);
+
+let friends = 'Mike';
+friends = 'John';
+friends = 'jim';
+alert(friends);
+
+
+let book = 'Hero of our Time';
+book = 1;
+alert(book);
+
+let myFavoriteFood = 'salmon';
+myFavoriteFood = 'chicken';
+myFavoriteFood = 'avocado';
+console.log(myFavoriteFood);
+
+let myFavoriteMovie = 'Sex and the City';
+myFavoriteMovie = 'Moulin Rouge';
+myFavoriteMovie = 'Catch me if you can';
+console.log(myFavoriteMovie);
+
+let followers = 1200;
+followers = 1579;
+followers = 1853;
+console.log(followers);
+
+let tomatoes = 4;
+let cucumbers = 3;
+let milk = 6;
+let cheese = 8;
+let wine = 25;
+let groceries = tomatoes + cucumbers + milk + cheese + wine;
+console.log(groceries);
+
+const SALATY = 100000;
+const FEDERAL_TAX = 12741;
+const PROVINCIAL_TAX = 15557;
+const OTHER_TAXES = 3631;
+
+const money = SALATY - FEDERAL_TAX - PROVINCIAL_TAX - OTHER_TAXES;
+console.log(money);
+
+
+let salary = 68071;
+const DAYS = 249
+salary = 72071;
+const dailySalary = salary / DAYS;
+console.log(dailySalary);
+
+let greeting = 'Welcome ';
+let userName = 'Steve! ';
+let text = 'I hope you like it here.';
+alert(greeting + userName + text); 
+
+
+let amountApples = 5;
+let amountOrandes = 10;
+let amountTomatoes = 3;
+alert(`You bought ${amountOrandes} oranges, ${amountApples} apples and ${amountTomatoes} tomatoes`);
+
+
+let groceries = 100;
+let clothes = 30;
+let toys = 20;
+let sum = groceries + clothes + toys;
+
+let message = `You spend ${sum} $.`;
+alert(message);
+
+
+let salary = 3000;
+let rent = 1000;
+let groceries = 500;
+
+let money = salary - rent - groceries;
+let message = `You have ${money}$ left per month.`
+alert(message);
+*/
+
+let text = 'I love CSS';
+let result = text.replace('love CSS', 'hate JS');
+alert(result);
+
+const myName = 'Ира';
+console.log(myName);
